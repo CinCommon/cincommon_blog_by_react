3483dcad59c03b56e83a1e856dbefcbb676a3433
CinCommon <452495748@qq.com>
2019-07-30 00:24:23 +0800

    1. zoom font-size

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import 'style/normalize.css'
 import 'style/iconfont/iconfont.css'
 import 'style/animation.css'
 import './index.scss'
 import BlogRouter from './router'
 import store from './store';
+import 'style/normalize.css'
 
 ReactDOM.render(
   <Provider store={store}>
